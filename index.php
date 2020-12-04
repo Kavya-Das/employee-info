6c04918ce8b7d1a1bb7b1ba5d6237385475b52a8
Kavya-Das <kavyasree.das@gmail.com>
2020-12-04 11:51:37 +0530

    added script

--- index.php
+++ index.php
@@ -5,26 +5,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Employee Info</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
 </head>
-<body>
+<body class="backcolor">
     
  <table class="table">
      <h1>Employee Information</h1>
      <tr>
          <td>Employee Name:</td>
-         <td><input type="text" class="form-control"></td>
+         <td><input type="text" id="name" class="form-control"></td>
      </tr>
      <tr>
          <td>Home:</td>
-         <td><input type="text" class="form-control"></td>
+         <td><input type="text" id="address" class="form-control"></td>
      </tr>
      <tr>
          <td>Designation:</td>
-         <td><input type="text" class="form-control"></td>
+         <td><input type="text" id="designation" class="form-control"></td>
      </tr>
      <tr>
          <td>Salary:</td>
-         <td><input type="text" class="form-control"></td>
+         <td><input type="text" id="salary" class="form-control"></td>
      </tr>
      <tr>
          <td>Address:</td>
@@ -32,16 +33,15 @@
      </tr>
      <tr>
          <td>Email:</td>
-         <td><input type="text" class="form-control"></td>
+         <td><input type="text" id="email" class="form-control"></td>
      </tr>
      <tr>
          <td>Phone:</td>
-         <td><input type="text" class="form-control"></td>
+         <td><input type="text" id="phone" class="form-control"></td>
      </tr>
      <tr>
          <td>Gender:</td>
-         <td><input type="radio" class="form-control" name="gender">Male </td>
-          <td><input type="radio" class="form-control" name="gender" >Female</td>
+         <td><input type="radio"  id="gender" name="gender">Male<input type="radio"  id="gender" name="gender" >Female</td>
      </tr>
      <tr>
          <td><input type="checkbox" class="form-control"></td>
@@ -49,8 +49,11 @@
      </tr>
      <tr>
          <td></td>
-         <td><button class="btn btn-success">Register</button></td>
+         <td><button onclick="clickme()" class="btn btn-success">Register</button></td>
      </tr>
- </table>   
+ </table>
+ <script src="js\script.js">
+         
+ </script>   
 </body>
 </html>
---
+++ js/script.js
@@ -0,0 +1,16 @@
+function clickme(){
+    var getname=document.getElementById("name").value
+    var getaddress=document.getElementById("address").value
+    var getdesignation=document.getElementById("designation").value
+    var getsalary=document.getElementById("salary").value
+    var getemail=document.getElementById("email").value
+    var getphone=document.getElementById("phone").value
+    var getgender=document.getElementById("gender").value
+    console.log(getname)
+    console.log(getaddress)
+    console.log(getdesignation)
+    console.log(getsalary)
+    console.log(getemail)
+    console.log(getphone)
+    console.log(getgender)
+}
